87f8add422f0320c060152139be3c6d1c99cc29f
johneyder <john.kazz@gmail.com>
2024-07-13 23:32:05 -0500

    init config sqlfluff

--- src/db/seed.sql
+++ src/db/seed.sql
@@ -1,37 +1,36 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE IF NOT EXISTS users (
-    id       TEXT PRIMARY KEY
-                  NOT NULL,
-    email    TEXT NOT NULL,
-    password TEXT NOT NULL
-);-- Products
+    id text PRIMARY KEY,
+    email text UNIQUE NOT NULL,
+    pass text NOT NULL
+);
+
 
 DROP TABLE IF EXISTS products;
 
 CREATE TABLE IF NOT EXISTS products (
-    id        TEXT      PRIMARY KEY
-                        NOT NULL,
-    title     TEXT      NOT NULL,
-    price     INT       NOT NULL,
-    cost      INT       NOT NULL,
-    create_at TEXT      DEFAULT CURRENT_TIMESTAMP
-                        NOT NULL,
-    update_at TIMESTAMP
+    id text PRIMARY KEY
+    NOT NULL,
+    title text NOT NULL,
+    price int NOT NULL,
+    cost int NOT NULL,
+    create_at text DEFAULT current_timestamp
+    NOT NULL,
+    update_at timestamp
 );-- Comments
 
 DROP TABLE IF EXISTS comments;
 
 CREATE TABLE IF NOT EXISTS comments (
-    id         TEXT PRIMARY KEY
-                    NOT NULL,
-    comment    TEXT NOT NULL,
-    product_id TEXT NOT NULL,
-    create_at  TEXT DEFAULT CURRENT_TIMESTAMP
-                    NOT NULL,
+    id text PRIMARY KEY NOT NULL,
+    body text NOT NULL,
+    product_id text NOT NULL,
+    create_at text DEFAULT current_timestamp
+    NOT NULL,
     FOREIGN KEY (
         product_id
     )
     REFERENCES products (id) ON UPDATE NO ACTION
-                             ON DELETE CASCADE
+    ON DELETE CASCADE
 );
